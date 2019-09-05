c5c24d49ea561c8f8a8ba503735ea256be5d2b8a
Fahim Ahmed <fahim152@gmail.com>
2019-09-05 14:07:28 +0600

    joke and website added

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -9,7 +9,7 @@ class BotController extends Controller
 {
     public function bot(Request $request)
     {
-
+        
         $data = $request->all();
         //fetching access token from db based on sent app token.
         //$accessToken = Setting::where('fb_app_name', $request['hub_verify_token'])->pluck('token')->first();
@@ -29,79 +29,128 @@ class BotController extends Controller
         $senderId =  $data['entry'][0]['messaging'][0]['sender']['id'];
         $messageText =  isset($data['entry'][0]['messaging'][0]['message']['text']) ? $data['entry'][0]['messaging'][0]['message']['text'] : '';
         $postback = isset($data['entry'][0]['messaging'][0]['postback']['payload']) ? $data['entry'][0]['messaging'][0]['postback']['payload']: '';
-        $attach = false;
-        $answer = ''; 
-        if($messageText !== ""){
+        $messagingArray = $data['entry'][0]['messaging'][0];
+
+        if($messageText !== ''){
             $answer = Chat::where('message_like','LIKE',"%$messageText%")->pluck('reply_with')->first();
-            if($answer == null || $answer == ""){
-                $answer="I'm afraid :( I can't understand what you have just said. Do you want me to send an image? reply with yes/no";
+      
+            if(empty($answer)){
                
+                $answer = 'I dont understand what you have just said';
+            }
+          
+            $response_json = '{
+                "recipient": {
+                    "id": "'.$senderId.'"
+                },
+                "message": {
+                    "text": "'.$answer.'"
+                }
+            }';
+
+                $this->sendMessage($accessToken, $response_json);
+
+        }
+      
+
+        if($postback == 'telljoke'){
+            $joke = json_decode(file_get_contents('http://api.icndb.com/jokes/random'), true);
+            $joke_get = $joke['value']['joke'];
+             
+                $response_json = '{
+                    "recipient": {
+                        "id": "'.$senderId.'"
+                    },
+                    "message": {
+                        "text": "'.$joke_get.'"
+                    }
+                }';
+
+                    $this->sendMessage($accessToken, $response_json);
+
+                    $response_json = '{
+                        "recipient": {
+                            "id": "'.$senderId.'"
+                        },
+                        "message": {
+                            "attachment":{
+                                "type":"template",
+                                "payload":{
+                                    "template_type":"button",
+                                    "text":"Need another joke ?",
+                                    "buttons": [
+                                        {
+                                            "type": "postback",
+                                            "title": "Hell yeah !",
+                                            "payload": "telljoke"
+                                        },
+                                        {
+                                            "type":"web_url",
+                                            "url":"http://www.thispersondoesnotexist.com",
+                                            "title":"Show website"   
+                                        }
+                                       
+                                    ]
+                                }
+                            }
+                        }
+                    }';
+    
+                        $this->sendMessage($accessToken, $response_json);
+        }    
+            if(isset($messagingArray['postback'])){
+                if($messagingArray['postback']['payload'] == 'first_hand_shake'){
+                 
+                    $message = "Hello Dear, Welcome to our page. why dont you just check our services or the website";
+                    $response_json = '{
+                        "recipient": {
+                            "id": "'.$senderId.'"
+                        },
+                        "message": {
+                            "text": "'.$message.'"
+                        }
+                    }';
+
+                    $this->sendMessage($accessToken, $response_json);
+
+                    $joke_json = '{
+                        "recipient":{
+                            "id":"'.$senderId.'"
+                        },
+                        "message": {
+                            "attachment":{
+                                "type":"template",
+                                "payload":{
+                                    "template_type":"button",
+                                    "text":"You can View our website",
+                                    "buttons": [
+                                        {
+                                            "type": "postback",
+                                            "title": "Tell me a joke",
+                                            "payload": "telljoke"
+                                        },
+                                        {
+                                            "type":"web_url",
+                                            "url":"http://www.thispersondoesnotexist.com",
+                                            "title":"Show website"   
+                                        }
+                                       
+                                    ]
+                                }
+                            }
+                        }
+                     }';
+                     $this->sendMessage($accessToken, $joke_json);
+                }
                 
             }
-            if($messageText == "yes"){
-                $attach = true;
-            }elseif($messageText == "no"){
-                $dd =  $this->sendMessagePostBack($accessToken, $senderId);
-                print_r($dd);
-                die();
-            }   
-        }
-
-        if($attach){
-            $response = [
-                'recipient' => [ 'id' => $senderId ],
-                 'message' => [ 'attachment' => [ 'type' => 'image', 'payload' => [ 'url' => 'https://www.google.com.bd/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'] ]],
-            ];
-        }else{
-            $response = [
-                'recipient' => [ 'id' => $senderId ],
-                "message"   => [ "text" => $answer ],
-            ];
-
-            
-        }
        
-        $this->sendMessage($accessToken, $response);
+           
         
     }
         
-    
-    public function sendMessage($accessToken,$response){
-        $ch = curl_init('https://graph.facebook.com/v4.0/me/messages?access_token='.$accessToken);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-        $result = curl_exec($ch);
-        curl_close($ch);
-    }
-
-    public function sendMessagePostBack($accessToken,$senderId){
-        $response_json = '{
-            "recipient":{
-                "id":"'.$senderId.'"
-            },
-            "message": {
-                "attachment":{
-                    "type":"template",
-                    "payload":{
-                        "template_type":"button",
-                        "text":"You can View our website";
-                        "buttons": [
-                            {
-                                "type":"web_url",
-                                "utl":"www.daraz.com",
-                                "title":"Show website"   
-                            },
-                            {
-                                "type":"postback",
-                                "title":"Start Chatting",
-                                "payload":"USER_DEFINED_PAYLOAD"
-                            }
-                        ]
-                    }
-                }
-            }
-         }';
+    public function sendMessage($accessToken, $response_json){
+       
         $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $response_json);
@@ -112,67 +161,4 @@ class BotController extends Controller
         return $result;
     }
 }
-// greeting messge sending along with button template, pending work. 
-    //     $response_greeting = [
-    //         'recipient' => [ 'id' => $senderId ],
-    //         'message' => [ 
-    //                     'attachment' => ['type' => 'template', 
-    //                                         'payload' => [
-    //                                         'template_type' => 'generic',
-    //                                         'elements' => [
-    //                                             'title' => "kitten",
-    //                                             'subtitle' => 'cute kitten pic',
-    //                                             'image_url' => 'https://placekitten.com/g/200/200',
-    //                                             'button' => [
-    //                                                 'type' => 'web_url',
-    //                                                 "url"   => 'https://placekitten.com/g/200/200',
-    //                                                 "title" => "Show kitten",
-    //                                             ],
-    //                                                 'type' => 'postback',
-    //                                                 'title' => 'I like this',
-    //                                                 "payload" => "User " + $senderId + " likes kitten " + 'https://placekitten.com/g/200/200',
 
-    //                                                 ]
-    //                                         ] 
-    //                                     ] 
-
-    //                         ],
-        
-            
-    //                      ];
-
-
-
-    // }
-
-
-// Another approach
-
-// $data = $request->all();
-//
-//         $id            = $data["entry"][0]["messaging"][0]["sender"]["id"];
-//         $senderMessage = $data["entry"][0]["messaging"][0]['message'];
-
-
-//         if (!empty($senderMessage)) {
-//             $this->sendTextMessage($id, "Hi buddy");
-//         }
-//     }
-//     private function sendTextMessage($recipientId, $messageText)
-//     {
-//         $messageData = [
-//             "recipient" => [
-//                 "id" => $recipientId,
-//             ],
-//             "message"   => [
-//                 "text" => $messageText,
-//             ],
-//         ];
-//         $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN"));
-
-//         curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
-//         curl_setopt($ch, CURLOPT_POST, true);
-//         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
-//         curl_exec($ch);
-//         curl_close($ch);
-//     }
